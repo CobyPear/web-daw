473362e2d6416fa1d6394fd88b1be3c7ff83ffab
_Pear <63015754+CobyPear@users.noreply.github.com>
2022-06-17 20:03:05 -0500

    Mute button on slider

--- src/lib/Slider.svelte
+++ src/lib/Slider.svelte
@@ -1,6 +1,34 @@
-<script>
+<script lang="ts">
   let amplitude = 75;
+  let lastAmplitude: number;
+  export let track: string;
+
+  const handleMute = (e: Event) => {
+    if (e.target instanceof Element) {
+      const currentState = e.target.innerHTML;
+      if (currentState === '🔊') {
+        lastAmplitude = amplitude;
+        amplitude = 0;
+      } else {
+        amplitude = lastAmplitude;
+      }
+      e.target.innerHTML = currentState === '🔇' ? '🔊' : '🔇';
+    }
+  };
 </script>
 
 <!-- slider -->
-<input class="-rotate-90" type="range" min="0" max="100" />
+<div class="flex flex-col">
+  <label for="slider">
+    {track}
+  </label>
+  <span>mute</span>
+  <button class="text-4xl" on:click={handleMute}>🔊</button>
+</div>
+<input
+  id="slider"
+  class="-rotate-90"
+  type="range"
+  min="0"
+  max="100"
+  bind:value={amplitude} />
--- src/lib/SliderBay.svelte
+++ src/lib/SliderBay.svelte
@@ -2,9 +2,8 @@
   import Slider from './Slider.svelte';
 </script>
 <section class="bg-purple-500 h-[25vh] w-fit p-5 mt-8 ml-8 flex">
-  SliderBay
-  <Slider />
-  <Slider />
-  <Slider />
-  <Slider />
+  <Slider track="Track 1" />
+  <Slider track="Track 2" />
+  <Slider track="Track 3" />
+  <Slider track="Track 4" />
 </section>
